16ccd352a2b08134cc21b921d9c38aaade057f3e
HsynHsyn <hakanhakanozdemir35@gmail.com>
2025-06-24 11:31:49 +0100

    BDD Last

---
+++ src/test/java/projectEvri/uiAutomation/step_definitions/selenium1.java
@@ -0,0 +1,77 @@
+package projectEvri.uiAutomation.step_definitions;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selenium1 {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setupMethod(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.get("https://www.google.com");
+        driver.get("https://practice.cydeo.com");
+}
+
+    @Test
+    public void test01(){
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Google";
+
+        try{
+            driver.findElement(By.id("L2AGLb")).click();
+        }catch(Exception e){
+            System.out.println("Button to accept cookies not found");
+        }
+
+
+        Assertions.assertEquals(actualTitle, expectedTitle);
+
+    }
+    
+    @Test
+    public void test02() throws InterruptedException{
+
+        String expectedLinkText = "A/B Testing";
+        WebElement ABTestingButton = driver.findElement(By.linkText("A/B Testing"));
+        ABTestingButton.click();
+        driver.navigate().back();
+        Assertions.assertEquals("Practice", driver.getTitle());
+
+        Thread.sleep(2000); // Wait for 2 seconds to observe the back navigation
+        //WebElement googleSearchBox = driver.findElement(By.name("q"));
+       // googleSearchBox.sendKeys("Amscreen");
+
+        List<WebElement> listOfLinks = driver.findElements(By.tagName("a"));
+
+        for(WebElement each:listOfLinks){
+            System.out.println(each.getText());
+            System.out.println(each.getAttribute("href"));
+        }
+
+
+
+    }
+
+
+    @AfterEach
+    public void tearDownMethod(){
+        driver.quit();
+    }
+
+
+    
+}   
---
+++ src/test/java/projectEvri/uiAutomation/step_definitions/testStepDefs.java
@@ -0,0 +1,78 @@
+package projectEvri.uiAutomation.step_definitions;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class testStepDefs {
+
+   WebDriver driver1;
+
+   @BeforeEach
+   public void setupMethod(){
+    driver1 = new ChromeDriver();
+    driver1.manage().window().maximize();
+    driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+    driver1.get("https://amscreen.com/");
+   }
+
+   @Test
+    public void test01() {
+
+    String actualTitle = driver1.getTitle();
+    String expectedTitle = "Amscreen | Digital Signage Specialists | IoT and Digital Screen Solutions";
+
+    Assertions.assertEquals(actualTitle, expectedTitle);
+
+    WebElement popUpButton = driver1.findElement(By.xpath("//button[text()='Accept']"));
+    popUpButton.click();
+    
+    WebElement element = driver1.findElement(By.xpath("//div[@class='header-btn']//a[@class='default-btn green']"));
+    
+    element.click();
+
+    }
+
+    @Test
+    public void test02(){
+
+        Actions actions = new Actions(driver1);
+
+        WebElement popUpButton = driver1.findElement(By.xpath("//button[text()='Accept']"));
+        popUpButton.click();
+
+        WebElement blackGetInTouchButton = driver1.findElement(By.xpath("(//div[@class='cell medium-6 link-align']//a[@class='default-btn black'])[2]"));
+        actions.moveToElement(blackGetInTouchButton).perform();
+
+        WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(10));
+        wait.withTimeout(Duration.ofSeconds(10));
+        
+        blackGetInTouchButton.click();
+
+        
+
+
+    }
+
+
+
+   @AfterEach
+   public void tearDownMethod(){
+    driver1.quit();
+   }
+
+
+
+       
+}
